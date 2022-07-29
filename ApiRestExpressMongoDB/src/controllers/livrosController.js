9fa30b81ca97151b796b0fd62afcf1f76f2ff484
Henrique Andrade <henrique.ccda@gmail.com>
2022-07-29 12:58:07 -0300

    Get e put por id.

--- ApiRestExpressMongoDB/src/controllers/livrosController.js
+++ ApiRestExpressMongoDB/src/controllers/livrosController.js
@@ -1,10 +1,23 @@
 import livros from '../models/Livro.js';
 
 class LivroController {
+
     static listarLivros = (req, res) => {
         livros.find((err, livros) => {
             res.status(200).json(livros);
         });
+    }
+
+    static listarLivroPorId = (req, res) => {
+        const id = req.params.id;
+
+        livros.findById(id, (err, livros) => {
+            if(err) {
+                res.status(400).send({message: `${err.message} - Id do livro ão localizado.`});
+            } else {
+                res.status(200).send(livros);
+            }
+        })
     }
 
     static cadastrarLivro = (req, res) => {
@@ -19,7 +32,18 @@ class LivroController {
         })
     }
 
+    static atualizarLivro = (req, res) => {
+        const id = req.params.id;
 
+        livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if(!err) {
+                res.status(200).send({message: 'Livro atualizado com sucess'});
+            } else {
+                res.status(500).send({message: err.message});
+            }
+
+        })
+    }
 }
 
 export default LivroController
